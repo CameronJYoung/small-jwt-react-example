ef511d3ec1c9776cf54f3e026fffc4403d5e3d22
CameronJYoung <cameronjyoung123@gmail.com>
2021-04-06 19:27:28 +0100

    removed unused require

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const jsonwebtoken = require('jsonwebtoken');
+
 const port = process.env.API_PORT;
 
 // Initialize app
