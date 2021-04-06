9356b8817863bd09b1ce67c1b521839ad1f9e499
CameronJYoung <cameronjyoung123@gmail.com>
2021-04-06 19:24:37 +0100

    removed unused routes from js file

--- server/app/routes/users.routes.js
+++ server/app/routes/users.routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const users = require('../controllers/users.controller.js');
 
 module.exports = (app) => {
-	router.post('/', users.create);
 	router.get('/', users.getAll);
 	router.delete('/:id', users.delete);
 	router.get('/:id', users.getById);
